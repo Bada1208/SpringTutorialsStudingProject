3c932dc8235e0c9947040f2530f34b9735ac1b32
Bada1208 <sysoievbogdan@gmail.com>
2020-11-20 05:17:20 +0100

    introduction done

--- 1_introduction/src/main/java/com/sysoiev/introduction/SpringHelloWorld.java
+++ 1_introduction/src/main/java/com/sysoiev/introduction/SpringHelloWorld.java
@@ -1,4 +1,14 @@
 package com.sysoiev.introduction;
 
 public class SpringHelloWorld {
+    private String helloMessage;
+
+    public void getHelloMessage() {
+        System.out.println("Hello message: " + helloMessage);
+    }
+
+    public void setHelloMessage(String helloMessage) {
+        this.helloMessage = helloMessage;
+    }
+
 }
