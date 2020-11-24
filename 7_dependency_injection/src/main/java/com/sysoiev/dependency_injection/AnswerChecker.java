e0faa5dc4f0f9530e3f3cf2e1b7b07f865a26c18
Bada1208 <sysoievbogdan@gmail.com>
2020-11-24 10:40:11 +0100

    add dependency injection

---
+++ 7_dependency_injection/src/main/java/com/sysoiev/dependency_injection/AnswerChecker.java
@@ -0,0 +1,12 @@
+package com.sysoiev.dependency_injection;
+
+public class AnswerChecker {
+
+    public AnswerChecker() {
+        System.out.println("AnswerChecker constructor...");
+    }
+
+    public void checkAnswer() {
+        System.out.println("Here we are checking answer....");
+    }
+}
---
+++ 7_dependency_injection/src/main/java/com/sysoiev/dependency_injection/Exam.java
@@ -0,0 +1,18 @@
+package com.sysoiev.dependency_injection;
+
+public class Exam {
+    private AnswerChecker answerChecker;
+
+    public AnswerChecker getAnswerChecker() {
+        return answerChecker;
+    }
+
+    public void setAnswerChecker(AnswerChecker answerChecker) {
+        this.answerChecker = answerChecker;
+        System.out.println("We`ve just set answerChecker using setter.");
+    }
+
+    public void examCheck() {
+        answerChecker.checkAnswer();
+    }
+}
