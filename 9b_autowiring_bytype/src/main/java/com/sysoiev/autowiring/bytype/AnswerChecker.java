dc161c3eb0eba59af153dd3ed9fb6f3102b63b0f
Bada1208 <sysoievbogdan@gmail.com>
2020-11-25 09:07:38 +0100

    add autowiring by type

---
+++ 9b_autowiring_bytype/src/main/java/com/sysoiev/autowiring/bytype/AnswerChecker.java
@@ -0,0 +1,12 @@
+package com.sysoiev.autowiring.bytype;
+
+public class AnswerChecker {
+
+    public AnswerChecker() {
+        System.out.println("AnswerChecker constructor...");
+    }
+
+    public void checkAnswer() {
+        System.out.println("Here we are checking answer....");
+    }
+}
