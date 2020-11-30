9409804297e17ec6258f1b47b44ceb40e1869bb2
Bada1208 <sysoievbogdan@gmail.com>
2020-11-30 10:18:13 +0100

    add event handler

---
+++ 12_event_handler/src/main/java/com/sysoiev/event_handler/MessageRunner.java
@@ -0,0 +1,15 @@
+package com.sysoiev.event_handler;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MessageRunner {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context =
+                new ClassPathXmlApplicationContext("message-event-handling-config.xml");
+        context.start();
+        Message message = (Message) context.getBean("message");
+        System.out.println(message);
+        context.stop();
+    }
+}
